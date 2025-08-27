597c27805f9c405c4964daf671624f0cbf51b1bc
George Dang <53052793+gtdang@users.noreply.github.com>
2025-08-27 12:58:24 -0400

    refactor: update Hero component to use title and description props for better readability

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -66,22 +66,12 @@ export default async function AboutUs() {
 
     return (
       <div className="w-full">
-        <div className="relative w-full flex flex-col">
-          <div className="bg-blue-navbar">
-            <Hero image={"/images/hero-subroutes.jpeg"}>
-              <div className="relative flex-1 flex items-start w-full px-6 md:px-24 bg-gradient-to-t from-black/0 via-black/10 to-black/65 z-5">
-                <div className="absolute top-[12%] flex flex-col text-white space-y-6 w-[80vw]">
-                  <TextAnimate className="font-bold text-6xl md:text-8xl">
-                    About Us
-                  </TextAnimate>
-                  <p className="text-4xl font-semibold leading-[1.5]">
-                    We empower genomic discovery through expert data analysis
-                    and investigator support.
-                  </p>
-                </div>
-              </div>
-            </Hero>
-          </div>
+        <div className="relative w-full flex flex-col bg-blue-navbar">
+          <Hero
+            image={"/images/hero-subroutes.jpeg"}
+            title="About Us"
+            description="We empower genomic discovery through expert data analysis and investigator support."
+          />
         </div>
 
         {/* About */}
