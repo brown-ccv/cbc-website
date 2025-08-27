b5aaac0c881fb5d5884d5753774bf8b7426c1e45
George Dang <53052793+gtdang@users.noreply.github.com>
2025-08-27 12:58:29 -0400

    refactor: update Services component to use title and description props in Hero for improved readability

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -67,23 +67,12 @@ function renderServiceCards(
 export default async function Services() {
   return (
     <div className="w-full">
-      <div className="relative w-full flex flex-col">
-        <div className="bg-blue-navbar">
-          <Hero image={"/images/hero/about-2.png"}>
-            <div className="relative flex-1 flex items-start w-full px-24 bg-gradient-to-t from-black/0 via-black/50 to-black/65 z-5">
-              <div className="absolute top-[12%] flex flex-col text-white space-y-6 w-[80vw]">
-                <TextAnimate className="font-bold text-6xl md:text-8xl">
-                  Services
-                </TextAnimate>
-                <p className="text-4xl font-semibold leading-[1.5]">
-                  From office hours to collaborations, the CBC provides a range
-                  of services to support Brown&apos;s genomic research
-                  community.
-                </p>
-              </div>
-            </div>
-          </Hero>
-        </div>
+      <div className="relative w-full flex flex-col bg-blue-navbar">
+        <Hero
+          image={"/images/hero/about-2.png"}
+          title="Services"
+          description="From office hours to collaborations, the CBC provides a range of services to support Brown's genomic research community."
+        />
       </div>
 
       {/* Office Hours and Support */}
