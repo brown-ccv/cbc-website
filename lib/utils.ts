8706486525e3cda4e5e111aeba55221b93cf8165
George Dang <53052793+gtdang@users.noreply.github.com>
2025-08-27 12:15:33 -0400

    refactor: update FeaturedCarousel component to use props and improve structure

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -10,23 +9,40 @@ export const tagColors = [
   "keppel",
   "sunglow",
   "purple",
-  "sky",
-  "amber",
-  "emerald",
   "blue",
-  "rose",
-  "cyan",
-  "lime",
+  "pink",
+  "red",
 ] as const
 
-export type TagColor = typeof tagColors[number]
+export type TagColor = (typeof tagColors)[number]
 
 export const getColorForTag = (tag: string): TagColor => {
-  const hash = Array.from(tag).reduce((acc, char) => acc + char.charCodeAt(0), 0)
+  const hash = Array.from(tag).reduce(
+    (acc, char) => acc + char.charCodeAt(0),
+    0
+  )
   return tagColors[hash % tagColors.length]
 }
 
 export const scrollToID = (targetId: string) => {
   const targetElement = document.getElementById(targetId)
   return targetElement?.scrollIntoView({ behavior: "smooth" })
+}
+
+/**
+ * Cleans up strings, removing underscores and uppercases first letter
+ * Type safe for none strings (that may get passed in through content)
+ *
+ * @param {string} str The string to be cleaned
+ */
+export const humanize = (str: string | null | undefined): string => {
+  if (typeof str !== "string" || str === null) {
+    return ""
+  }
+  const cleanStr = str.replace(/_/g, " ")
+  if (cleanStr.length === 0) {
+    return ""
+  }
+  const upperFirst = cleanStr.charAt(0).toUpperCase() + cleanStr.slice(1)
+  return upperFirst
 }
