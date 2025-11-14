9bcea867f71a80013c33c8c84f1fc7d212168cfd
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-11-14 16:14:04 -0500

    fix: remove preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  presets: [],
   darkMode: "media", // or 'class'
   theme: {
     accentColor: ({ theme }) => ({
