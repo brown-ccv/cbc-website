cd700c655ee85a3830c1513a60189645bf49ad15
ashley <ashley_lee@brown.edu>
2025-11-18 11:03:16 -0500

    refactor: updates markdown styling to use ccv website conventions, creates wip about us page with first 2 sections converted to mdx

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -1,40 +1,50 @@
 import React from "react"
-import CCVBars from "@/components/assets/CCVBars"
+import CBCBars from "@/components/assets/CBCBars"
+import { cn } from "@/lib/utils"
 
 interface SectionHeaderProps {
   title: string
   align?: "left" | "center"
   bars?: boolean
   icon?: React.ReactNode
+  subHeader?: React.ReactNode
   className?: string
+  titleClassName?: string
 }
 
 export const SectionHeader: React.FC<SectionHeaderProps> = ({
   title,
   align = "center",
   bars = true,
+  subHeader,
   icon,
   className,
+  titleClassName,
 }) => {
-  const isCentered = align === "center"
-
   return (
     <div
-      className={`${isCentered ? "mb-16 text-center" : "text-left"} ${className || ""}`}
+      className={cn(
+        `not-prose flex flex-col pb-6 ${align === "center" ? "items-center" : "max-w-1/2 items-center lg:items-start"}`,
+        className
+      )}
     >
-      <div
-        className={`flex flex-col ${isCentered ? "items-center" : "items-start"}`}
-      >
-        {bars && (
-          <div className="pt-4">
-            <CCVBars />
-          </div>
+      {bars && <CBCBars />}
+      <h2
+        className={cn(
+          "pb-2 tracking-tighter",
+          icon ? "flex" : "",
+          titleClassName
         )}
-        <h2 className="flex items-center text-4xl font-semibold tracking-tighter text-black">
-          {icon && <span className="mr-3">{icon}</span>}
-          {title}
-        </h2>
-      </div>
+        aria-label={title}
+      >
+        {icon && (
+          <span className="mr-3" aria-hidden="true">
+            {icon}
+          </span>
+        )}
+        {title}
+      </h2>
+      {subHeader && <div className="space-y-6 pt-4">{subHeader}</div>}
     </div>
   )
 }
