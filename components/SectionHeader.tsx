a8325fbe39d2359f353f3fb64a56ddc3c880d3bf
Heather Yu <35639529+hetd54@users.noreply.github.com>
2026-03-11 12:10:18 -0400

    fix: add back sectionheader

---
+++ components/SectionHeader.tsx
@@ -0,0 +1,50 @@
+import React from "react"
+import { CBCBars } from "@/components/assets/CBCBars"
+import { cn } from "@/lib/utils"
+
+interface SectionHeaderProps {
+  title: string
+  align?: "left" | "center"
+  bars?: boolean
+  icon?: React.ReactNode
+  subHeader?: React.ReactNode
+  className?: string
+  titleClassName?: string
+}
+
+export function SectionHeader({
+  title,
+  align = "center",
+  bars = true,
+  subHeader,
+  icon,
+  className,
+  titleClassName,
+}: SectionHeaderProps) {
+  return (
+    <div
+      className={cn(
+        `not-prose flex flex-col ${align === "center" ? "items-center" : "items-center lg:items-start"}`,
+        className
+      )}
+    >
+      {bars && <CBCBars />}
+      <h2
+        className={cn(
+          "pb-2 tracking-tighter",
+          icon ? "flex" : "",
+          titleClassName
+        )}
+        aria-label={title}
+      >
+        {icon && (
+          <span className="mr-3" aria-hidden="true">
+            {icon}
+          </span>
+        )}
+        {title}
+      </h2>
+      {subHeader && <div className="space-y-6 pt-4">{subHeader}</div>}
+    </div>
+  )
+}
